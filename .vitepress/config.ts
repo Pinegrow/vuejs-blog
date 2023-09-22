0365a29e50334d9b957d5f34ca4e7a1831046dd1
Akayy <techakayy@gmail.com>
2023-09-22 21:19:39 +1000

    DP keywords

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -43,7 +43,7 @@ export default defineConfig({
           cssPath: './theme/style.css'
           // themePath: false, // Set to false so that Design Panel is not used
           // restartOnConfigUpdate: true,
-          // restartOnThemeUpdate: true,
+          restartOnThemeUpdate: true,
         }
         //...
       })
